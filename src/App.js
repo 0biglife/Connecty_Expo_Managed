94ff2860a4a85ae4fcf7eebeb200a03f2dae387c
0biglife <kimchoon42@gmail.com>
2021-09-27 19:34:07 +0900

    Navigation Control

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ThemeProvider } from 'styled-components/native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const ScreenA = ({ navigation }) => {
+
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        ScreenA
+      </Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : '#0f0' })}
+      >
+        <Text style={styles.text}>
+          Go to Screen B
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const ScreenB = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        ScreenB
+      </Text>
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barSyle="dark-content"
-        backgroundColor={theme.background}
-      />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen_A" component={ScreenA} />
+        <Stack.Screen name="Screen_B" component={ScreenB} />
+      </Stack.Navigator>
+    </NavigationContainer>
 
-    </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //     <Text>fuck you RN</Text>
+    //     <StatusBar backgroundColor={theme.background} />
+    // </ThemeProvider>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -22,5 +64,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    margin: 10,
   },
 });
